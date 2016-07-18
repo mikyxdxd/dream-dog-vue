1752d372c8d2a1136897f837b80604f6994427a0
AlexLi88 <alexlxt88@gmail.com>
2016-07-18 14:10:47 -0700

    add email icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,6 +7,7 @@ module.exports = Vue.extend({
                 footerimg: require('../../resource/footer_ilustrastion.png'),
                 appleStore: require('../../resource/app_store.png'),
                 gooogleStore:require('../../resource/google_play.png'),
+                emailicon: require('../../resource/email_icon.png'),
                 fb: require('../../resource/facebook.png'),
                 ins: require('../../resource/instagram.png'),
                 twit:require('../../resource/twitter.png'),
