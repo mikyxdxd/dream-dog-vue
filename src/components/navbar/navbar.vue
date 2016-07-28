44da3610608c97bfdeabdb7876840638911338f5
shilianghong <shiliang.hong01@gmail.com>
2016-07-28 09:37:43 -0700

    -update

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -2,12 +2,18 @@
     <div id="nav_bar">
         <div class="one_nav" :class="{'selected':currentView=='introduction'}" v-on:click="toPosition('introduction')">
             <div class="title">Introduction</div>
+            <div class="process_bar_container"></div>
+            <div class="process_bar" id="title_pb"></div>
         </div>
         <div class="one_nav" :class="{'selected':currentView=='shelter'}" v-on:click="toPosition('shelter')">
             <div class="title">Shelter</div>
+          <div class="process_bar_container"></div>
+          <div class="process_bar" id="shelter_pb"></div>
         </div>
         <div class="one_nav" :class="{'selected':currentView=='feature'}" v-on:click="toPosition('feature')">
             <div class="title">Feature</div>
+            <div class="process_bar_container"></div>
+            <div class="process_bar" id="feature_pb"></div>
         </div>
         <div class="one_nav" :class="{'selected':currentView=='contact'}" v-on:click="toPosition('contact')">
             <div class="title">Contact</div>
@@ -65,14 +71,18 @@
             this.feature_p = $('#feature').position().top + $('#feature').height() * 1 / 2;
             this.contact_p = $('#contact').position().top + 400;
             var self = this;
-
             window.onscroll = function () {
 
                 if ($('body').scrollTop() <= self.contact_p) {
 
-                    if ($('body').scrollTop() <= self.feature_p) {
+                    if ($('body').scrollTop() <= $('#feature').position().top) {
 
-                        if ($('body').scrollTop() <= self.shelter_p) {
+                        if ($('body').scrollTop() <= $('#shelter').position().top) {
+
+
+                            $('#title_pb').width(
+                               $('body').scrollTop() / ($('#shelter').position().top)
+                               * $($('.process_bar_container')[0]).width());
 
                             self.$set('currentView', 'introduction');
                             $('#nav_bar').css('margin-top', '-25px');
@@ -80,6 +90,14 @@
 
                         } else {
 
+
+                            $('#title_pb').width($($('.process_bar_container')[0]).width());
+
+                            console.log( $('body').scrollTop() / ($('#feature').position().top))
+
+                            $('#shelter_pb').width(
+                            $('body').scrollTop() / ($('#feature').position().top)
+                            * $($('.process_bar_container')[0]).width());
                             self.$set('currentView', 'shelter');
                             $('#nav_bar').css('margin-top', '-75px');
                         }
@@ -93,6 +111,7 @@
 
                 } else {
 
+                    $('#title_pb,#shelter_pb,#feature_pb').width($($('.process_bar_container')[0]).width());
                     self.$set('currentView', 'contact');
                     $('#nav_bar').css('margin-top', '-175px');
 
