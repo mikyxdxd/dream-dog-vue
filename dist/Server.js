0ef5edd510c3ac33050ec080dde65cded66d13fa
shilianghong <shiliang.hong01@gmail.com>
2016-08-02 16:30:30 -0700

    -commit

--- dist/Server.js
+++ dist/Server.js
@@ -1,78 +1,70 @@
-/**
- * Created by Alex on 2016-07-26.
- */
-
-
 'use strict'
 
-
-const express = require('express'),
+var express = require('express'),
   compress = require('compression'),
-  engines = require('consolidate'),
-  defaultPort = 8080,
-  bodyParser = require('body-parser'),
-  nodemailer = require('nodemailer'),
   app = module.exports = express(),
-  http = require('http');
+  engines = require('consolidate'),
+  defaultPort = 9000;
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.use(compress({
+  filter: function (req, res) {
+    return true;
+  }
+}));
 
-app.engine('html', engines.mustache),
 
-  app.set('view engine', 'html');
+var oneDay = 86400000;
+app.use(express['static'](__dirname, {maxAge: oneDay * 10}));
+var bodyParser = require('body-parser');
+app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-app.use(express['static'](__dirname,{}));
+
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded(
-  {extended: true}
-));
-// app.use(compress({
-//   filter: function (req, res) {
-//     return true;
-//   }
-// }));
+app.post('/email', function (req, res, next) {
+  // console.log("POST: ");
+  // console.log(req.body);
+  let mailOpts, smtptrans, sendText;
+  sendText = req.body.message + "\n\n\n\n\n" + "from: " + req.body.user + " Email:" + req.body.email;
+  mailOpts = {
+    from: req.body.user + ":" + req.body.email,
+    to: "contact@dreamdogapp.com",
+    subject: 'Dreamdog contact form',
+    text: sendText,
+  };
+  smtptrans = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: "imagemapapp@gmail.com",
+      pass: "Whitespot"
+    }
+  });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  smtptrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      // console.log("EMAIL ERROR");
+      // console.log(error);
+      res.json({title: "DreamDog -Contact", msg: "Error Occured", err: true, page: 'contact'});
+      //res.render('contact', {title: "DreamDog -Contact", msg:"Error Occured", err: true, page:'contact'});
+    } else {
+      //console.log(response);
+      res.json({title: 'DreamDog - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact'});
+      //res.render('contact', { title: 'DreamDog - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+    }
+  });
+
 });
 
-
-app.post('/email', function(req,res, next){
-    // console.log("POST: ");
-    // console.log(req.body);
-    let mailOpts, smtptrans, sendText;
-    sendText = req.body.message + "\n\n\n\n\n" + "from: " + req.body.user + " Email:" + req.body.email;
-    mailOpts = {
-        from: req.body.user + ":" + req.body.email,
-        to: "contact@dreamdogapp.com",
-        subject: 'Dreamdog contact form',
-        text: sendText,
-    };
-    smtptrans = nodemailer.createTransport({
-        service: 'Gmail',
-        auth:{
-            user: "imagemapapp@gmail.com",
-            pass: "Whitespot"
-        }
-    });
-
-    smtptrans.sendMail(mailOpts, function(error, response){
-        if (error){
-            // console.log("EMAIL ERROR");
-            // console.log(error);
-            res.json({title: "DreamDog -Contact", msg:"Error Occured", err: true, page:'contact'});
-            //res.render('contact', {title: "DreamDog -Contact", msg:"Error Occured", err: true, page:'contact'});
-        }else{
-            //console.log(response);
-            res.json({ title: 'DreamDog - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' });
-            //res.render('contact', { title: 'DreamDog - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
-        }
-    });
-
-});
-
-
-console.log("Listening port", defaultPort);
 http.createServer(app).listen(defaultPort);
+//https.createServer(credentials,app).listen(443);
+
+// app.listen(defaultPort);
+console.log('Server starts on ' + defaultPort);
